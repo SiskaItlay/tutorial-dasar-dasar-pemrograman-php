5aeca6059381daf558f9414e40e2aee6134672fd
siskaitlay <siskaasso111@gmail.com>
2023-11-28 09:46:31 +0900

    tambah comments.php

---
+++ commenst.php
@@ -0,0 +1,15 @@
+<?php
+    // contoh komentar single-line di php
+
+    # ini juga contoh komentar single-line di php
+
+    /*
+    kalau yang ini merupakan contoh
+    komentar multiple-lines
+    yang bisa diterapkan
+    di dalamsebuah file php
+    */
+
+    echo "PHP Comments";
+
+?>
---
+++ php-html.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kombinasi PHP & HTML</title>
+</head>
+<body>
+    
+    <h1>Kombinasi PHP & HTML</h1>
+
+</body>
+</html>
